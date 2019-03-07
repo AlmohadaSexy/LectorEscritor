760d2032ffcfe8f3fbeb10d4a7ba212a25ff516e
Jorge Moreno <jorge.velez.moreno@gmail.com>
2019-03-07 23:21:01 +0100

    Creacion Basica del proceso de Escritor y estructura del Buffer
    Se deben de modificar los parametros principales tanto del Escritor como
    del Buffer.

--- Lector Escritor/src/Escritor.java
+++ Lector Escritor/src/Escritor.java
@@ -1,14 +1,52 @@
+import java.util.Random;
 
 public class Escritor extends Thread{
+	
 	public Escritor() {
 		start();
+	}
+	
+	
+	private void escribe() throws InterruptedException {
+		
+		Random rdmNum = new Random();
+		int numP = rdmNum.nextInt(999) + 1;
+		
+		int sleepTime = rdmNum.nextInt(250-25+1)+25; // Entre 25 y 250ms
+		
+		sleep(sleepTime);
+		
+		
+		System.out.println("Escritor: ISBN "+numP+" escrito.");
+		
+		// Agregamos al Buffer
+		
+		Buffer.getLibreria().add(numP);
+		
+		
 	}
 	
 	
 	@Override
 	public void run() {
 		while(true) {
+			if(Buffer.getLibreria().size() == Buffer.bSize) {
+				System.out.println("Escritor. Nada mas que escribir, compadre.");
+			}
 			
+			try {
+				Buffer.getsNoLleno().acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				escribe();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			Buffer.getsNoVacio().release();
 		}
 	}
 }
